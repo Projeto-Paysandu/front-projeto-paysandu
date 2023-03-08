8ea69694241f647d52a345d1a94578844723549a
Rubson Lima <limarubson@hotmail.com>
2023-03-08 17:48:51 -0300

    Login screen.

---
+++ app/src/main/java/com/example/front_projeto_paysandu/screen/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.front_projeto_paysandu.screen.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.front_projeto_paysandu.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
