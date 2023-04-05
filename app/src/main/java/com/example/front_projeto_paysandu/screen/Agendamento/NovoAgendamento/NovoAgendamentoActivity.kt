b03d7b44aab9832da73bcb95fa1ae106b4052348
Rubson Lima <limarubson@hotmail.com>
2023-04-05 15:48:37 -0300

    Criada a tela Novo Agendamento.

---
+++ app/src/main/java/com/example/front_projeto_paysandu/screen/Agendamento/NovoAgendamento/NovoAgendamentoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.front_projeto_paysandu.screen.Agendamento.NovoAgendamento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.front_projeto_paysandu.R
+
+class NovoAgendamentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_agendamento)
+    }
+}
