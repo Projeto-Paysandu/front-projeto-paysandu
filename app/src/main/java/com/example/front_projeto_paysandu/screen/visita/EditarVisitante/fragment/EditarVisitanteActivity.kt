33058c50b2c7c8e1dcc64b3edf27bb13d3ab5b8d
Rubson Lima <limarubson@hotmail.com>
2023-04-12 17:06:12 -0300

    Tela de edição de visitante criada.

---
+++ app/src/main/java/com/example/front_projeto_paysandu/screen/visita/EditarVisitante/fragment/EditarVisitanteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.front_projeto_paysandu.screen.visita.EditarVisitante.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarVisitanteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_visitante2)
+    }
+}
