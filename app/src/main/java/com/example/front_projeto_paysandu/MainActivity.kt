f0f745ce8e56e0924e928ff70ca3c5bca3e21914
Rubson Lima <limarubson@hotmail.com>
2023-04-12 10:46:49 -0300

    Tela Garagem criada.

--- app/src/main/java/com/example/front_projeto_paysandu/MainActivity.kt
+++ app/src/main/java/com/example/front_projeto_paysandu/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.front_projeto_paysandu
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.front_projeto_paysandu.screen.Agendamento.AgendamentosActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val descricao = findViewById<View>(R.id.textCPF) as TextView
     }
 
     fun telaAgendamento(view: View?) {
